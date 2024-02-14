1acdbd19b6fa14051ccadaba8e304f6dd11b2c36
Berriop <dortizortiz021gmail.com>
2024-02-14 13:53:25 -0500

    TODA LA CLASE

---
+++ poo/app.py
@@ -0,0 +1,7 @@
+from poo.figuras import Circulo, Punto
+
+circulo_1: Circulo = Circulo(5, Punto(2, 3))
+punto_1: Punto = Punto(2, 3)
+
+print(circulo_1.contiene(punto_1))
+
---
+++ poo/figuras.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Punto:
+    def __init__(self, x: int, y: int):
+        self.x: int = x
+        self.y: int = y
+
+
+class Circulo:
+    def __init__(self, radio: int, centro: Punto):
+        self.radio: int = radio
+        self.centro: Punto = centro
+
+    def calcular_area(self) -> float:
+        return math.pi * self.radio ** 2
+
+    def contiene(self, punto: Punto) -> bool:
+        return (punto.x - self.centro.x) ** 2 + (punto.y - self.centro.y) ** 2 <= self.radio ** 2
